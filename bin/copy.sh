6c2bf4a49f9a57a68a82c27cda315bf200724cec
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-03-22 17:42:38 +0100

    Make copy script fail with message when tmux-yank is not installed

--- bin/copy.sh
+++ bin/copy.sh
@@ -3,6 +3,12 @@
 set -e
 
 tmux_yank_helpers="$HOME/.tmux/plugins/tmux-yank/scripts/helpers.sh"
+
+if [ ! -f "$tmux_yank_helpers" ]; then
+    echo "tmux-yank is not installed!"
+    exit 1
+fi
+
 source "$tmux_yank_helpers"
 
 eval "$(clipboard_copy_command)"
