f7ecaa8b842612272545ff622432eee48b6bf3e3
Dietrich Moerman <dietrich.moerman@gmail.com>
2020-10-31 14:13:47 +0100

    Set `ctagsp` alias to index tags within Python environments as well
    Aside from indexing the current repository, also index any Python site
    packages installed within a virtualenv or Conda environment. This
    expands the scope of tags reachable from within Neovim for Python
    projects.

--- shell.aliases.sh
+++ shell.aliases.sh
@@ -1,3 +1,4 @@
+alias ctagsp='ctags . ${VIRTUAL_ENV:-$CONDA_PREFIX}/lib/python*/site-packages'
 alias largest='du -hsx * | sort -rh | head -15'
 alias ls='ls --color=auto'
 alias rgi='rg --ignore-vcs'
