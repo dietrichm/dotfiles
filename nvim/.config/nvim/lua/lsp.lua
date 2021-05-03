3925ef0ca1ab2dae7761ac3ad3c3baf02e163dfa
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-05-03 11:53:09 +0200

    Disable virtual text for LSP diagnostics for now

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,3 +1,9 @@
 require'lspconfig'.gopls.setup{
     cmd = {"gopls", "-remote=auto"}
 }
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false
+    }
+)
