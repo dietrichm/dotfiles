6933db6968de1d1cb1505300763db92b2056e767
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-05-05 16:25:34 +0200

    Disable signs for LSP diagnostics
    One day this might replace ALE.

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -21,6 +21,7 @@ lspconfig.gopls.setup{
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
-        virtual_text = false
+        virtual_text = false,
+        signs = false,
     }
 )
