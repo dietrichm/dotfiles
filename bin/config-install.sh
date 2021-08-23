86d9b650b6d8e7c8582db6556194503640db6625
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-08-23 08:51:57 +0200

    Remove installing redundant terminfo file for tmux-256color

--- bin/config-install.sh
+++ bin/config-install.sh
@@ -23,12 +23,6 @@ else
     selected_packages=("${all_packages[@]}")
 fi
 
-if [[ " ${selected_packages[*]} " =~ " tmux " ]]; then
-    # Compile and install terminfo file for Tmux.
-    tic "$MY_CONFIG_ROOT/etc/tmux-256color.terminfo" \
-        || echo -e "Skipped compiling terminfo for Tmux.\n"
-fi
-
 # Install Python dependencies.
 pip3 install --user -U -r "$MY_CONFIG_ROOT/etc/requirements.txt"
 echo
