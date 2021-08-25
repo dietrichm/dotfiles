69a46b36d6cf57a99254cbda36109b34102880a5
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-08-25 12:38:40 +0200

    Keep PHP indentation working

--- nvim/.config/nvim/lua/treesitter.lua
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -1,6 +1,6 @@
 require('nvim-treesitter.configs').setup{
     highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = {'php'},
     },
 }
