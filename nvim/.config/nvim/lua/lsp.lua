be4945ce14a8deb7e818926afea94df3296eeedf
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-11-24 16:16:36 +0100

    Re-enable incremental sync for LSP

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -25,9 +25,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local flags = {
-  allow_incremental_sync = false,
-}
+local flags = {}
 
 lspconfig.gopls.setup{
   cmd = {"gopls", "-remote=auto"},
