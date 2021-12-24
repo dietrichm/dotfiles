fd8554b13ed458cd6f4853490900c912c58ebffc
Dietrich Moerman <dietrich.moerman@gmail.com>
2021-12-24 10:25:22 +0100

    Have alias to list Go module updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,4 +1,5 @@
 alias ctagsp='ctags . ${VIRTUAL_ENV:-$CONDA_PREFIX}/lib/python*/site-packages'
+alias go-updates='go list -m -mod=mod -u -f "{{if .Update}}{{.}}{{end}}" all'
 alias info='info --vi-keys'
 alias largest='du -hsx * | sort -rh | head -15'
 alias ls='ls --color=auto'
