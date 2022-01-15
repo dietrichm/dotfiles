1581f01add9fb44e917dc38419ce2d7cffc6dee7
Dietrich Moerman <dietrich.moerman@gmail.com>
2022-01-15 16:56:56 +0100

    Let cmp_nvim_lsp update LSP capabilities

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -25,9 +25,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities.textDocument.completion.completionItem.snippetSupport = true
-
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 local flags = {}
 
 lspconfig.gopls.setup{
