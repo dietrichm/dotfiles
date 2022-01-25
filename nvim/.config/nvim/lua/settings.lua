22f4b2a9198e8d4f32342d75d60c1d371bcf8c95
Dietrich Moerman <dietrich.moerman@gmail.com>
2022-01-25 18:45:23 +0100

    Reinstate hunk navigation with gitsigns

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -6,4 +6,14 @@ require('gitsigns').setup{
     topdelete    = {hl = 'GitSignsDelete', text = '‾', numhl='GitSignsDeleteNr', linehl='GitSignsDeleteLn'},
     changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
   },
+  on_attach = function(bufnr)
+    local function map(mode, l, r, opts)
+      opts = opts or {}
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
+    end
+
+    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+  end
 }
