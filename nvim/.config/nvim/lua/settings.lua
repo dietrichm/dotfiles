e4e87f93e6ac3d57371dae5d75388d423101d2f7
Dietrich Moerman <dietrich.moerman@gmail.com>
2022-04-18 21:39:48 +0200

    Configure gitsigns hunk navigation as documented

--- nvim/.config/nvim/lua/settings.lua
+++ nvim/.config/nvim/lua/settings.lua
@@ -7,13 +7,24 @@ require('gitsigns').setup{
     changedelete = {hl = 'GitSignsChange', text = '~', numhl='GitSignsChangeNr', linehl='GitSignsChangeLn'},
   },
   on_attach = function(bufnr)
+    local gs = package.loaded.gitsigns
+
     local function map(mode, l, r, opts)
       opts = opts or {}
       opts.buffer = bufnr
       vim.keymap.set(mode, l, r, opts)
     end
 
-    map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
-    map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+    map('n', ']c', function()
+      if vim.wo.diff then return ']c' end
+      vim.schedule(function() gs.next_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
+
+    map('n', '[c', function()
+      if vim.wo.diff then return '[c' end
+      vim.schedule(function() gs.prev_hunk() end)
+      return '<Ignore>'
+    end, {expr=true})
   end
 }
