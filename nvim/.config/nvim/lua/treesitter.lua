673f32e243390ece5386403d9ebae2c36a3a4fd1
Dietrich Moerman <dietrich.moerman@gmail.com>
2022-04-20 12:15:39 +0200

    Ensure Treesitter parsers are installed

--- nvim/.config/nvim/lua/treesitter.lua
+++ nvim/.config/nvim/lua/treesitter.lua
@@ -1,4 +1,18 @@
 require('nvim-treesitter.configs').setup{
+  ensure_installed = {
+    'css',
+    'go',
+    'html',
+    'javascript',
+    'json',
+    'markdown',
+    'php',
+    'python',
+    'toml',
+    'typescript',
+    'vim',
+    'yaml',
+  },
   highlight = {
     enable = true,
     additional_vim_regex_highlighting = {'php'},
