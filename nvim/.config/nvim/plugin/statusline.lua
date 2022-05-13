1c069c0df9b4b777e80f234210cb2866418ef367
Dietrich Moerman <dietrich.moerman@gmail.com>
2022-05-13 14:54:28 +0200

    Remove ALE support in custom statusline plug-in

--- nvim/.config/nvim/plugin/statusline.lua
+++ nvim/.config/nvim/plugin/statusline.lua
@@ -1,5 +1,3 @@
-local use_null_ls = vim.env.NVIM_USE_NULL_LS == '1'
-
 function _G.SpellStatusLine()
   if not vim.o.spell then
     return ''
@@ -9,41 +7,20 @@ function _G.SpellStatusLine()
 end
 
 local function LSPStatusCounts()
-  local has_force_ale_linting, force_ale_linting = pcall(vim.api.nvim_get_var, 'force_ale_linting')
-
-  if has_force_ale_linting and force_ale_linting == 1 then
-    return { errors = 0, warnings = 0 }
-  end
-
   return {
     errors = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.ERROR }),
     warnings = #vim.diagnostic.get(0, { severity = vim.diagnostic.severity.WARN }),
   }
 end
 
-local function ALEStatusCounts()
-  if use_null_ls then
-    return { errors = 0, warnings = 0 }
-  end
-
-  local buffer = vim.api.nvim_buf_get_number(0)
-  local counts = vim.fn['ale#statusline#Count'](buffer)
-
-  return {
-    errors = counts.error + counts.style_error,
-    warnings = counts.warning + counts.style_warning,
-  }
-end
-
 function _G.DiagnosticStatusLine()
   local lsp = LSPStatusCounts()
-  local ale = ALEStatusCounts()
 
-  if lsp.errors + ale.errors + lsp.warnings + ale.warnings == 0 then
+  if lsp.errors + lsp.warnings == 0 then
     return ''
   end
 
-  return string.format('❌%d ❗%d', lsp.errors + ale.errors, lsp.warnings + ale.warnings)
+  return string.format('❌%d ❗%d', lsp.errors, lsp.warnings)
 end
 
 vim.o.statusline = [[%<%f %y%{v:lua.SpellStatusLine()}%m%r %{v:lua.DiagnosticStatusLine()} %=%-14.(%l,%c%V%) %P]]
