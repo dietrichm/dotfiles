191e3ebe8600b2fda5d2c058a153ba3bade550a8
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-06-03 14:08:15 +0200

    Use <Leader>q to populate location list with diagnostics

--- nvim/.config/nvim/lua/dietrichm/lsp.lua
+++ nvim/.config/nvim/lua/dietrichm/lsp.lua
@@ -19,6 +19,7 @@ local on_attach = function(_, bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+  vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, 'n', '<Leader>lf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
 end
 
