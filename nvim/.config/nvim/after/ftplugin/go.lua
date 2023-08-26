62c68f3a8e0ecfb0386aaf75fe2f47eceedaeffe
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-08-26 10:18:31 +0200

    Replace Go snippets with abbreviations
    Relates to #129.

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -8,3 +8,16 @@ vim.keymap.set('n', '<Leader>gr', '<Plug>(go-run)', opts)
 vim.keymap.set('n', '<Leader>gt', '<Plug>(go-test)', opts)
 vim.keymap.set('n', '<Leader>gv', '<Plug>(go-vet)', opts)
 vim.keymap.set('n', '<Leader>u', '<Plug>(go-import)', opts)
+
+function GoMockgen()
+  return string.format(
+    '//go:generate go run github.com/golang/mock/mockgen -source %s -destination %s -package %s',
+    vim.fn.expand('%:t'),
+    vim.fn.expand('%:t:r') .. '_mock.go',
+    vim.fn.expand('%:h:t')
+  )
+end
+
+vim.cmd.iabbrev('mctrl', [[ctrl := gomock.NewController(t)]])
+vim.cmd.iabbrev('<expr>', 'mockgen', [[v:lua.GoMockgen()]])
+vim.cmd.iabbrev('dump', [[fmt.Printf("%+v\n", ]])
