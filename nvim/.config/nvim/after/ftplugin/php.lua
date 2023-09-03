337ef433a39814a56e7fce5d26043f1fbbb4100e
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-09-03 11:33:31 +0200

    Use colorcolumn at +1 and 121 for any PHP file

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -1,20 +1,9 @@
 vim.opt_local.textwidth = 80
-vim.opt_local.colorcolumn = { '121' }
+vim.opt_local.colorcolumn:append('121')
 vim.bo.commentstring = '// %s'
 
 -- Include and gf file matching.
 vim.opt_local.includeexpr = [[substitute(v:fname, '^/', '', '')]]
-
-local augroup = vim.api.nvim_create_augroup('vimrc_php', { clear = true })
-
--- Colour column 81 only for PHP source files and tests.
-vim.api.nvim_create_autocmd('BufEnter', {
-  pattern = '*/{app,domain,src,tests}/*.php',
-  group = augroup,
-  callback = function()
-    vim.opt_local.colorcolumn:append('+1')
-  end
-})
 
 -- Partial namespace from path.
 vim.cmd.iabbrev('<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
