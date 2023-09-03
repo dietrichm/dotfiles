4fc9691e5a75a7fc12e2790baca2d80fc9db98a3
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-09-03 10:25:56 +0200

    Remove remaining null-ls diagnostics
    Relates to #138.

--- nvim/.config/nvim/lua/dietrichm/lsp.lua
+++ nvim/.config/nvim/lua/dietrichm/lsp.lua
@@ -126,8 +126,6 @@ local null_ls = require('null-ls')
 null_ls.setup {
   on_attach = on_attach,
   sources = {
-    null_ls.builtins.diagnostics.flake8,
-    null_ls.builtins.diagnostics.vint,
     null_ls.builtins.formatting.pint,
   }
 }
