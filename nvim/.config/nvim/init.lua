85a637d86de140221348a90023acfc664f740687
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-09-03 11:29:50 +0200

    Have commands to write without triggering autocommands

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -209,5 +209,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 -- Global abbreviations.
 vim.cmd.iabbrev('<expr>', 'uuid', [[luaeval('io.open("/proc/sys/kernel/random/uuid"):read()')]])
 
+-- Write without triggering autocommands.
+vim.api.nvim_create_user_command('Nw', [[:noautocmd w]], {})
+vim.api.nvim_create_user_command('Nwa', [[:noautocmd wa]], {})
+
 -- Lua plugins.
 require('dietrichm')
