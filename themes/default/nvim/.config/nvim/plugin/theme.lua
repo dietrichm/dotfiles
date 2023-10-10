458e87998d7101d72d942dea1e7b9611e32d85e3
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-10-10 13:52:15 +0200

    Highlight LSP references in Molokai colour scheme

--- themes/default/nvim/.config/nvim/plugin/theme.lua
+++ themes/default/nvim/.config/nvim/plugin/theme.lua
@@ -1,1 +1,11 @@
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'molokai',
+  group = 'vimrc',
+  callback = function()
+    vim.cmd.highlight { 'LspReferenceText', 'guibg=#4c4745' }
+    vim.cmd.highlight { 'LspReferenceRead', 'guibg=#4c4745' }
+    vim.cmd.highlight { 'LspReferenceWrite', 'guibg=#4c4745' }
+  end
+})
+
 vim.cmd.colorscheme('molokai')
