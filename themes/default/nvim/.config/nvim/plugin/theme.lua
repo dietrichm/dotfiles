932139ba49c054d444da529f435b5f93cad43617
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-12-10 13:46:31 +0100

    Split generic and Molokai highlight autocmds

--- themes/default/nvim/.config/nvim/plugin/theme.lua
+++ themes/default/nvim/.config/nvim/plugin/theme.lua
@@ -1,10 +1,17 @@
 vim.api.nvim_create_autocmd('ColorScheme', {
-  pattern = 'molokai',
+  pattern = '*',
   group = 'vimrc',
   callback = function()
     vim.cmd.highlight { 'link', 'LspReferenceText', 'IncSearch' }
     vim.cmd.highlight { 'link', 'LspReferenceRead', 'IncSearch' }
     vim.cmd.highlight { 'link', 'LspReferenceWrite', 'IncSearch' }
+  end,
+})
+
+vim.api.nvim_create_autocmd('ColorScheme', {
+  pattern = 'molokai',
+  group = 'vimrc',
+  callback = function()
     vim.cmd.highlight { 'link', 'WinBar', 'StatusLine', bang = true }
     vim.cmd.highlight { 'link', 'WinBarNC', 'StatusLineNC', bang = true }
   end,
