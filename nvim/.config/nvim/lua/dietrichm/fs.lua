61b9b908218b749d83d7088ff68e44893117f2ec
Dietrich Moerman <dietrich.moerman@gmail.com>
2023-12-12 19:38:54 +0100

    Set Oil keymaps like Telescope and avoid setting CTRL-H and CTRL-L

--- nvim/.config/nvim/lua/dietrichm/fs.lua
+++ nvim/.config/nvim/lua/dietrichm/fs.lua
@@ -7,6 +7,14 @@ oil.setup {
   view_options = {
     show_hidden = true,
   },
+  keymaps = {
+    ['<C-s>'] = false,
+    ['<C-v>'] = 'actions.select_vsplit',
+    ['<C-h>'] = false,
+    ['<C-x>'] = 'actions.select_split',
+    ['<C-l>'] = false,
+    ['<C-r>'] = 'actions.refresh',
+  },
 }
 
 vim.keymap.set('n', '-', [[:Oil<CR>]], { silent = true })
