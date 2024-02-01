43d58d8adb0545c85dd52ed76768e4bc163b11fa
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-02-01 20:02:30 +0100

    Have shell function to compress PDF with default settings

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -17,3 +17,13 @@ stt() {
 
     kitty @ set-tab-title "$title"
 }
+
+pdfcompress() {
+    if [ $# -lt 2 ]; then
+        echo "Usage: $0 <input-file> <output-file>"
+        return 1
+    fi
+
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/default -dQUIET -dNOPAUSE -dBATCH -sOutputFile="$2" "$1"
+    ls -lh "$1" "$2"
+}
