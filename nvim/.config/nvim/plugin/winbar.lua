482844dce3836908b41655cc2bce6d6f0dcf7f96
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-02-25 14:37:07 +0100

    Have treesitter powered 'winbar' for PHP and Go

---
+++ nvim/.config/nvim/plugin/winbar.lua
@@ -0,0 +1,41 @@
+-- luacheck: globals TreesitterWinbar
+
+local loaded, treesitter = pcall(require, 'nvim-treesitter')
+if not loaded then
+  return
+end
+
+function TreesitterWinbar()
+  local line = treesitter.statusline {
+    type_patterns = {
+      'function_declaration',
+      'method_declaration',
+    },
+  }
+
+  if line == nil then
+    return ''
+  end
+
+  return line
+end
+
+treesitter.define_modules {
+  vimrc_winbar = {
+    enable = true,
+    attach = function()
+      vim.wo.winbar = [[%{v:lua.TreesitterWinbar()}]]
+    end,
+    detach = function()
+      vim.wo.winbar = nil
+    end,
+    is_supported = function(language)
+      local languages = {
+        'go',
+        'php',
+      }
+
+      return vim.tbl_contains(languages, language)
+    end,
+  },
+}
