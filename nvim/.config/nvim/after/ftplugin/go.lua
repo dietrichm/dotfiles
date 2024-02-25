a0e0bacb9225972b953b22912f8d9d4a22f35990
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-02-25 12:50:39 +0100

    Remove unused vim-go keymaps

--- nvim/.config/nvim/after/ftplugin/go.lua
+++ nvim/.config/nvim/after/ftplugin/go.lua
@@ -2,19 +2,6 @@
 
 vim.opt_local.colorcolumn:append('100')
 
-local function map(mode, lhs, rhs)
-  vim.keymap.set(mode, lhs, rhs, { buffer = true, silent = true })
-end
-
--- Commands provided by vim-go.
-map('n', '<Leader>gb', '<Plug>(go-build)')
-map('n', '<Leader>gc', '<Plug>(go-coverage-toggle)')
-map('n', '<Leader>gi', '<Plug>(go-imports)')
-map('n', '<Leader>gr', '<Plug>(go-run)')
-map('n', '<Leader>gt', '<Plug>(go-test)')
-map('n', '<Leader>gv', '<Plug>(go-vet)')
-map('n', '<Leader>u', '<Plug>(go-import)')
-
 function GoMockgen()
   return string.format(
     '//go:generate go run github.com/golang/mock/mockgen -source %s -destination %s -package %s',
