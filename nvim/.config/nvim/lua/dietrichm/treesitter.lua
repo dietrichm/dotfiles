c7769790bc3fbb0733b8a1cd631b5d1fad708126
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-02-25 13:29:55 +0100

    Enable treesitter movement to outer/inner functions

--- nvim/.config/nvim/lua/dietrichm/treesitter.lua
+++ nvim/.config/nvim/lua/dietrichm/treesitter.lua
@@ -41,6 +41,22 @@ treesitter.setup {
         ['if'] = '@function.inner',
       },
     },
+    move = {
+      enable = true,
+      set_jumps = true,
+      goto_next_start = {
+        [']m'] = '@function.outer',
+      },
+      goto_next_end = {
+        [']M'] = '@function.outer',
+      },
+      goto_previous_start = {
+        ['[m'] = '@function.outer',
+      },
+      goto_previous_end = {
+        ['[M'] = '@function.outer',
+      },
+    },
   },
 }
 
