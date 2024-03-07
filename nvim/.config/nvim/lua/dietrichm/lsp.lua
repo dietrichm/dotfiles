c3db62bb2c82c5cef4846b0198b4574723d507ec
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-03-07 08:52:40 +0100

    Remove redundant Intelephense settings

--- nvim/.config/nvim/lua/dietrichm/lsp.lua
+++ nvim/.config/nvim/lua/dietrichm/lsp.lua
@@ -93,13 +93,6 @@ lspconfig.intelephense.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   flags = flags,
-  settings = {
-    intelephense = {
-      files = {
-        maxSize = 6000000,
-      },
-    },
-  },
 }
 
 lspconfig.phpactor.setup {
