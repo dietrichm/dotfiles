efefded660c48b3fcd87d9080c584bc77a028615
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-04-29 12:21:27 +0200

    Re-enable Go imports on save

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -179,7 +179,6 @@ vim.g.go_code_completion_enabled = 0
 vim.g.go_def_mapping_enabled = 0
 vim.g.go_doc_keywordprg_enabled = 0
 vim.g.go_fmt_fail_silently = 1
-vim.g.go_imports_autosave = 0
 vim.g.go_jump_to_error = 0
 vim.g.go_textobj_enabled = 0
 
