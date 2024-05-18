0558e3e6e46e07b9dced0772a27486e46d671aa0
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-05-18 21:24:06 +0200

    Configure <(S-)Tab> and <CR> for completion

--- nvim/.config/nvim/lua/dietrichm/completion.lua
+++ nvim/.config/nvim/lua/dietrichm/completion.lua
@@ -15,6 +15,28 @@ cmp.setup {
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
     ['<M-p>'] = function()
       if cmp.visible_docs() then
         cmp.close_docs()
