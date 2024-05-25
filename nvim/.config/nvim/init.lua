7c60fcbf07bca5e2dcae7ee5245d7cfc2b3757f4
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-05-25 12:54:26 +0200

    Install jsregexp

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -42,7 +42,10 @@ require('paq'):setup { verbose = true } {
   'hrsh7th/nvim-cmp',
   'hrsh7th/cmp-nvim-lsp',
   'saadparwaiz1/cmp_luasnip',
-  'L3MON4D3/LuaSnip',
+  {
+    'L3MON4D3/LuaSnip',
+    build = 'make install_jsregexp',
+  },
 
   -- Git.
   'tpope/vim-fugitive',
