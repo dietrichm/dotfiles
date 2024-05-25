98777c945aed6bd1507535e183cf3a6517010cb7
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-05-25 12:38:56 +0200

    Bold sign

--- themes/default/zsh/prompt.zsh
+++ themes/default/zsh/prompt.zsh
@@ -1,4 +1,4 @@
 # NvimLightBlue: #a6dbff
 # NvimLightGreen: #b3f6c0
 # NvimLightGrey4: #9b9ea4
-export PS1='%F{#9b9ea4}%* %F{#a6dbff}%~ %F{#b3f6c0}%# %f'
+export PS1='%F{#9b9ea4}%* %F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
