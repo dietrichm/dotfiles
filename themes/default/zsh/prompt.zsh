69e93986e3d403269b35b5265a5ae870d74e8e0e
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-06-07 13:02:44 +0200

    Improve kitty/zsh prompt colours to be Nvim alike

--- themes/default/zsh/prompt.zsh
+++ themes/default/zsh/prompt.zsh
@@ -1,4 +1,4 @@
 # NvimLightBlue: #a6dbff
 # NvimLightGreen: #b3f6c0
-# NvimLightGrey4: #9b9ea4
-export PS1='%F{#9b9ea4}%* %F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
+# NvimDarkGrey4: #4f5258
+export PS1='%F{#4f5258}%* %F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
