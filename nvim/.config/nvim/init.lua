879fe82ef2834d372267e7849808a8efd8b9faab
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-09-14 21:13:05 +0200

    Disable 'smartindent'

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -64,7 +64,6 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-vim.opt.smartindent = true
 vim.opt.hlsearch = false
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
