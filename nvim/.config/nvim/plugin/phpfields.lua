62d95ab01a3fdc48a8a6b38c5a35690ed3f833f3
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-10-04 15:53:01 +0200

    Have dirty plugin to fill PHP argument names (fields) in function call

---
+++ nvim/.config/nvim/plugin/phpfields.lua
@@ -0,0 +1,25 @@
+local function feedkeys(keys)
+  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(keys, true, false, true), 'm', false)
+end
+
+local function applyFields()
+  local params = vim.lsp.util.make_position_params()
+
+  vim.lsp.buf_request(0, 'textDocument/signatureHelp', params, function(_, result)
+    if not result or not result.signatures then
+      vim.notify('No signature found to apply PHP fields.', vim.log.levels.ERROR)
+      return
+    end
+
+    local label = result.signatures[1].label
+
+    -- Expect to be positioned on first argument with one argument per line.
+    for field in string.gmatch(label, '$([^ ,)]+)') do
+      feedkeys('I' .. field .. ': <Esc>j')
+    end
+  end)
+end
+
+vim.api.nvim_create_user_command('PhpFields', function()
+  applyFields()
+end, {})
