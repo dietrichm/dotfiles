0247dac064da684c001b96a5e174e9c2e28b3504
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-10-06 17:51:06 +0200

    Map `<Space>af` to apply PHP fields

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -5,3 +5,5 @@ vim.b.delimitMate_matchpairs = '(:),[:],{:}'
 
 vim.cmd.iabbrev('<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
 vim.cmd.iabbrev('<expr>', 'cls', [[expand('%:t:r')]])
+
+vim.keymap.set('n', '<Leader>af', [[:PhpFields<CR>]], { buffer = true, silent = true })
