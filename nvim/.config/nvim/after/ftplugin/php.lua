3ec676257931a0a809de11f3de7d435289f2efbf
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-10-06 17:50:43 +0200

    Re-organise php.lua

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -1,11 +1,7 @@
 vim.opt_local.matchpairs:remove('<:>')
+vim.opt_local.includeexpr = [[substitute(v:fname, '^/', '', '')]]
 vim.bo.commentstring = '// %s'
 vim.b.delimitMate_matchpairs = '(:),[:],{:}'
 
--- Include and gf file matching.
-vim.opt_local.includeexpr = [[substitute(v:fname, '^/', '', '')]]
-
--- Partial namespace from path.
 vim.cmd.iabbrev('<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
-
 vim.cmd.iabbrev('<expr>', 'cls', [[expand('%:t:r')]])
