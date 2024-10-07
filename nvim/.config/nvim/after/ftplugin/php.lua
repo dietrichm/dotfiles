b33024a396f1af8e293d55ad3b7b4fb7434ef89d
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-10-07 16:43:56 +0200

    Abbreviate `strict` in PHP

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -5,5 +5,6 @@ vim.b.delimitMate_matchpairs = '(:),[:],{:}'
 
 vim.cmd.iabbrev('<buffer>', '<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
 vim.cmd.iabbrev('<buffer>', '<expr>', 'cls', [[expand('%:t:r')]])
+vim.cmd.iabbrev('<buffer>', 'strict', [[declare(strict_types=1);]])
 
 vim.keymap.set('n', '<Leader>af', [[:PhpFields<CR>]], { buffer = true, silent = true })
