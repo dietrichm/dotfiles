d808daf5eb572581ffb304b2ab10d2b0896b1734
Dietrich Moerman <dietrich.moerman@gmail.com>
2024-12-04 10:08:44 +0100

    Fix running inline variable refactoring keymap

--- nvim/.config/nvim/lua/dietrichm/refactoring.lua
+++ nvim/.config/nvim/lua/dietrichm/refactoring.lua
@@ -5,5 +5,5 @@ end
 
 refactoring.setup()
 
-vim.keymap.set('n', '<Leader>rv', [[:Refactor inline_var]])
+vim.keymap.set('n', '<Leader>rv', [[:Refactor inline_var<CR>]])
 vim.keymap.set('x', '<Leader>rv', [[:Refactor extract_var ]])
