9ff53a4dc8f94b182b986f56a26eb4482376c38a
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-02-17 10:44:04 +0100

    Use zsh `$PROMPT` variable

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,4 +1,4 @@
 # NvimLightBlue: #a6dbff
 # NvimLightGreen: #b3f6c0
 # NvimDarkGrey4: #4f5258
-export PS1='%F{#4f5258}%* %F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
+export PROMPT='%F{#4f5258}%* %F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
