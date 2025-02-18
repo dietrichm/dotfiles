79714aa592ea065304a053b90cfe5a926fbd7ee8
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-02-18 09:47:57 +0100

    Increase time legibility

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,6 +1,6 @@
 # NvimLightBlue: #a6dbff
 # NvimLightGreen: #b3f6c0
-# NvimDarkGrey4: #4f5258
+# NvimLightGrey4: #9a9da4
 PROMPT='%F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
-RPROMPT='%F{#4f5258}%*%f'
+RPROMPT='%F{#9a9da4}%*%f'
 ZLE_RPROMPT_INDENT=0
