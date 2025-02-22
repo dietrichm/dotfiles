f80a1047dfdf5fd4ca05bd1818e50cebd09025e2
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-02-22 15:47:10 +0100

    Have commented POC for nvim 0.11 autocomplete

--- nvim/.config/nvim/lua/dotfiles/completion.lua
+++ nvim/.config/nvim/lua/dotfiles/completion.lua
@@ -4,6 +4,18 @@ vim.opt.pumwidth = 20
 
 if vim.fn.has('nvim-0.11') == 1 then
   vim.opt.completeopt:append('fuzzy')
+
+  -- vim.api.nvim_create_autocmd('LspAttach', {
+  --   group = vim.api.nvim_create_augroup('dotfiles_completion', { clear = true }),
+  --   callback = function(args)
+  --     local client_id = args.data.client_id
+  --     local bufnr = args.buf
+  --     vim.lsp.completion.enable(true, client_id, bufnr, { autotrigger = true })
+  --     vim.keymap.set('i', '<C-Space>', vim.lsp.completion.trigger, { buffer = bufnr })
+  --   end,
+  -- })
+  --
+  -- return
 end
 
 local loaded, cmp = pcall(require, 'cmp')
