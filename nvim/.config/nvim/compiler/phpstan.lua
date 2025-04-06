dc9e33c0ffe38cc484ceecce052f3e9de2df6d50
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-04-06 20:25:35 +0200

    Show PHPStan error identifiers

--- nvim/.config/nvim/compiler/phpstan.lua
+++ nvim/.config/nvim/compiler/phpstan.lua
@@ -1,4 +1,4 @@
 vim.b.current_compiler = 'phpstan'
 
-vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ --memory-limit=2G\ --no-progress\ --error-format=raw]])
-vim.cmd.CompilerSet([[errorformat=%f:%l:%m,%-GNote:\ %.%#]])
+vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ -v\ --memory-limit=2G\ --no-progress\ --error-format=raw]])
+vim.cmd.CompilerSet([[errorformat=%f:%l:%m,%-G%.%#]])
