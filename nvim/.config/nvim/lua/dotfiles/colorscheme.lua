91e454c64b2011c5db08fa788b5b4be5c264fc0a
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-04-26 13:44:04 +0200

    Extract `:highlight` to `hi`
    Using `vim.api.nvim_set_hl` is annoying because it does not retain
    existing properties.

--- nvim/.config/nvim/lua/dotfiles/colorscheme.lua
+++ nvim/.config/nvim/lua/dotfiles/colorscheme.lua
@@ -1,13 +1,14 @@
 local augroup = vim.api.nvim_create_augroup('dotfiles_colorscheme', { clear = true })
+local hi = vim.cmd.highlight
 
 vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = '*',
   group = augroup,
   callback = function()
-    vim.cmd.highlight { 'DiagnosticUnderlineError', 'gui=undercurl' }
-    vim.cmd.highlight { 'link', 'TelescopeNormal', 'NormalFloat' }
-    vim.cmd.highlight { 'link', 'TelescopeMatching', 'IncSearch' }
-    vim.cmd.highlight { 'link', 'TelescopeSelection', 'CursorLine' }
+    hi { 'DiagnosticUnderlineError', 'gui=undercurl' }
+    hi { 'link', 'TelescopeNormal', 'NormalFloat' }
+    hi { 'link', 'TelescopeMatching', 'IncSearch' }
+    hi { 'link', 'TelescopeSelection', 'CursorLine' }
   end,
 })
 
@@ -15,15 +16,15 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = 'default',
   group = augroup,
   callback = function()
-    vim.cmd.highlight { 'link', 'ColorColumn', 'CursorLine', bang = true }
-    vim.cmd.highlight { 'TreesitterContextBottom', 'gui=underline', 'guisp=NvimDarkGrey4' }
+    hi { 'link', 'ColorColumn', 'CursorLine', bang = true }
+    hi { 'TreesitterContextBottom', 'gui=underline', 'guisp=NvimDarkGrey4' }
     if vim.env.TERM == 'xterm-kitty' then
-      vim.cmd.highlight { '@comment', 'guifg=NvimLightGrey4', 'gui=italic' }
+      hi { '@comment', 'guifg=NvimLightGrey4', 'gui=italic' }
     end
-    vim.cmd.highlight { 'link', 'LspInlayHint', '@comment' }
-    vim.cmd.highlight { 'Added', 'guifg=NvimLightBlue' }
-    vim.cmd.highlight { 'Changed', 'guifg=NvimLightYellow' }
-    vim.cmd.highlight { 'FloatBorder', 'guifg=NvimDarkGrey4' }
+    hi { 'link', 'LspInlayHint', '@comment' }
+    hi { 'Added', 'guifg=NvimLightBlue' }
+    hi { 'Changed', 'guifg=NvimLightYellow' }
+    hi { 'FloatBorder', 'guifg=NvimDarkGrey4' }
   end,
 })
 
