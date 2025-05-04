e2e94af9fa7ce434389f1851f63210114c28bf38
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-05-04 18:12:21 +0200

    Remove `ns` PHP abbreviation
    Intelephense completes namespaces.

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -2,8 +2,6 @@ vim.opt_local.matchpairs:remove('<:>')
 vim.opt_local.includeexpr = [[substitute(v:fname, '^/', '', '')]]
 vim.bo.commentstring = '// %s'
 
-vim.cmd.iabbrev('<buffer>', '<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
-
 vim.keymap.set('n', 'grf', _G.PhpFields, { buffer = true })
 
 vim.keymap.set('n', '<Leader>ft', function()
