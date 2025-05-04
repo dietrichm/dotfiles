0197a605628046c21c3d3e102bdda38f90267a00
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-05-04 17:52:26 +0200

    Map `<Leader>ft` to create and open PHP test in vsplit

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -5,3 +5,14 @@ vim.bo.commentstring = '// %s'
 vim.cmd.iabbrev('<buffer>', '<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
 
 vim.keymap.set('n', 'grf', _G.PhpFields, { buffer = true })
+
+vim.keymap.set('n', '<Leader>ft', function()
+  local filename = vim.fn.expand('%:.')
+  if filename:match('^src/') == nil then
+    vim.notify('Not a source file.', vim.log.levels.ERROR)
+    return
+  end
+  filename = filename:gsub('^src/', 'tests/')
+  filename = filename:gsub('%.php$', 'Test.php')
+  vim.cmd.vsplit(filename)
+end, { buffer = true })
