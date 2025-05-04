c486deb2177c9088502b83507c7b31ba5544b4e9
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-05-04 14:11:58 +0200

    Remove redundant `cls` abbreviation and `:PhpFile`

--- nvim/.config/nvim/after/ftplugin/php.lua
+++ nvim/.config/nvim/after/ftplugin/php.lua
@@ -3,19 +3,5 @@ vim.opt_local.includeexpr = [[substitute(v:fname, '^/', '', '')]]
 vim.bo.commentstring = '// %s'
 
 vim.cmd.iabbrev('<buffer>', '<expr>', 'ns', [[substitute(tr(expand('%:p:h'), '/', '\'), '\v^.*\\\l[^\\]*\\?', '', '')]])
-vim.cmd.iabbrev('<buffer>', '<expr>', 'cls', [[expand('%:t:r')]])
 
 vim.keymap.set('n', 'grf', _G.PhpFields, { buffer = true })
-
-vim.api.nvim_buf_create_user_command(0, 'PhpFile', function()
-  local content = [[<?php
-
-declare(strict_types=1);
-
-namespace ns;
-
-final class cls
-{
-}]]
-  vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes('i' .. content .. '<Esc>', true, false, true), 'm', false)
-end, {})
