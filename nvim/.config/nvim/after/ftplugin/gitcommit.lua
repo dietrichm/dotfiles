43861fadd2de1e5d83dfb34bd5dfedc95fe737c3
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-05-23 13:43:47 +0200

    Simplify `bri`

--- nvim/.config/nvim/after/ftplugin/gitcommit.lua
+++ nvim/.config/nvim/after/ftplugin/gitcommit.lua
@@ -4,13 +4,7 @@ vim.b.editorconfig = false
 
 vim.keymap.set('ia', 'bri', function()
   local branch_name = vim.fn.system { 'git', 'rev-parse', '--abbrev-ref', 'HEAD' }
-  local match_start, match_end = vim.regex('[A-Z]\\+-[0-9]\\+'):match_str(branch_name)
-
-  if not match_start then
-    return '?'
-  end
-
-  return branch_name:sub(match_start + 1, match_end)
+  return branch_name:match('%u+%-%d+') or '?'
 end, { buffer = true, expr = true })
 
 vim.keymap.set('ia', 'cfs', function()
