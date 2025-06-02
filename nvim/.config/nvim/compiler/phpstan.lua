2d136ffb9ee503896b7bc2ba915c9a7409fed5fc
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-06-02 15:02:51 +0200

    Improve PHPStan arguments

--- nvim/.config/nvim/compiler/phpstan.lua
+++ nvim/.config/nvim/compiler/phpstan.lua
@@ -1,4 +1,4 @@
 vim.b.current_compiler = 'phpstan'
 
-vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ -v\ --memory-limit=2G\ --no-progress\ --error-format=raw]])
+vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ -nv\ --no-progress\ --error-format=raw]])
 vim.cmd.CompilerSet([[errorformat=%f:%l:%m,%-G%.%#]])
