3acba1d64ee543f2efa8db42bc432bd3264af097
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-15 18:14:11 +0200

    Fix verbose flag for PHPStan

--- nvim/.config/nvim/compiler/phpstan.lua
+++ nvim/.config/nvim/compiler/phpstan.lua
@@ -1,4 +1,4 @@
 vim.b.current_compiler = 'phpstan'
 
-vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ -nv\ --no-progress\ --error-format=raw]])
+vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ -n\ -v\ --no-progress\ --error-format=raw]])
 vim.cmd.CompilerSet([[errorformat=%f:%l:%m,%-G%.%#]])
