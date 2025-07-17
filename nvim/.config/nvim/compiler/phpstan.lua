3fcccb42f0cda7fe7575e32195d200481ec10e2f
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-17 20:37:10 +0200

    Replace Lua PHPStan compiler with vim's

--- nvim/.config/nvim/compiler/phpstan.lua
+++
@@ -1,4 +0,0 @@
-vim.b.current_compiler = 'phpstan'
-
-vim.cmd.CompilerSet([[makeprg=phpstan\ analyse\ -n\ -v\ --no-progress\ --error-format=raw]])
-vim.cmd.CompilerSet([[errorformat=%f:%l:%m,%-G%.%#]])
