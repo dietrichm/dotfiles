25390ce54e4025dbba56f2b2fc226738f1d10b2e
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-20 17:46:25 +0200

    Configure cfilter with mappings

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -1,2 +1,5 @@
 vim.opt_local.wrap = true
 vim.opt_local.signcolumn = 'auto'
+
+vim.keymap.set('n', "'", [[:Cfilter ]], { buffer = true })
+vim.keymap.set('n', '!', [[:Cfilter! ]], { buffer = true })
--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -7,6 +7,7 @@ vim.g.loaded_perl_provider = 0
 vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
+vim.cmd.packadd('cfilter')
 require('paq'):setup { verbose = true } {
   'echasnovski/mini.nvim',
   'hrsh7th/nvim-cmp',
