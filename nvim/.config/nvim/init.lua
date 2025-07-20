5673d66ff30ca0fe9b748eb26ba1b9251193ec99
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-20 15:54:37 +0200

    Sort options alphabetically

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -33,37 +33,31 @@ require('paq'):setup { verbose = true } {
 }
 -- Sort in {...} selection using :sort /[a-z-\/]\+/ ri.
 
-vim.opt.scrolloff = 2
-vim.opt.number = true
-vim.opt.fileformats = 'unix'
-vim.opt.showmode = true
-vim.opt.hlsearch = false
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.linebreak = true
-vim.opt.title = true
-vim.opt.titlestring = 'nvim ' .. vim.fs.basename(vim.env.PWD)
-vim.opt.list = true
-vim.opt.listchars = {
-  tab = '▏ ',
-  extends = '»',
-  precedes = '«',
-  trail = '·',
-  nbsp = '␣',
-}
-vim.opt.shortmess:append { I = true }
-vim.opt.spelllang = 'en_gb'
-vim.opt.undofile = true
-vim.opt.exrc = true
+vim.opt.colorcolumn = { '+1' }
+vim.opt.cpoptions:append { Z = true }
 vim.opt.cursorline = true
 vim.opt.cursorlineopt = 'number'
-vim.opt.colorcolumn = { '+1' }
-vim.opt.guicursor:append('a:blinkon500-blinkoff500')
+vim.opt.exrc = true
+vim.opt.fileformats = 'unix'
 vim.opt.guicursor:append('a:Cursor')
-vim.opt.signcolumn = 'yes'
-vim.opt.cpoptions:append { Z = true }
+vim.opt.guicursor:append('a:blinkon500-blinkoff500')
+vim.opt.hlsearch = false
+vim.opt.ignorecase = true
+vim.opt.linebreak = true
+vim.opt.list = true
+vim.opt.listchars = { tab = '▏ ', extends = '»', precedes = '«', trail = '·', nbsp = '␣' }
+vim.opt.number = true
 vim.opt.ruler = false
+vim.opt.scrolloff = 2
+vim.opt.shortmess:append { I = true }
+vim.opt.showmode = true
+vim.opt.signcolumn = 'yes'
+vim.opt.smartcase = true
+vim.opt.spelllang = 'en_gb'
 vim.opt.timeoutlen = 3000
+vim.opt.title = true
+vim.opt.titlestring = 'nvim ' .. vim.fs.basename(vim.env.PWD)
+vim.opt.undofile = true
 
 if vim.env.NVIM_EXTUI == '1' and vim.fn.has('nvim-0.12') == 1 then
   require('vim._extui').enable {}
