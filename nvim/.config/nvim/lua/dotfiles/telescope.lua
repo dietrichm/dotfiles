32dbd04e5fb34cdf4bd9fe1a16f435338e12db47
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-20 17:31:19 +0200

    Reimplement <Leader>sw using :grep

--- nvim/.config/nvim/lua/dotfiles/telescope.lua
+++ nvim/.config/nvim/lua/dotfiles/telescope.lua
@@ -73,9 +73,6 @@ map('n', '<Leader>O', function()
 end)
 map('n', '<Leader>b', builtin.buffers)
 map('n', '<Leader>fh', builtin.oldfiles)
-map('n', '<Leader>sw', function()
-  builtin.grep_string { word_match = '-w' }
-end)
 map('n', '<Leader>sg', builtin.live_grep)
 map('n', '<Leader>gs', function()
   builtin.git_status { previewer = vim.go.columns >= 200 }
