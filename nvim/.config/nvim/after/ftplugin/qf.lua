ab2c614952f7aa6978f1fea1ff520df33985af9c
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-21 20:22:51 +0200

    Disable quickfix wrap by default and set <Leader>w to toggle

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -2,7 +2,7 @@ vim.opt_local.cursorlineopt = 'both'
 vim.opt_local.number = false
 vim.opt_local.showbreak = '    '
 vim.opt_local.signcolumn = 'auto'
-vim.opt_local.wrap = true
+vim.opt_local.wrap = false
 
 vim.keymap.set('n', "'", [[:Cfilter ]], { buffer = true })
 vim.keymap.set('n', '!', [[:Cfilter! ]], { buffer = true })
