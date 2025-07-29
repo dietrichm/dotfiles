28cd3cefb54cae838207337a0587a18d664f1434
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-07-29 15:50:11 +0200

    Configure and enable LuaLS for nvim

---
+++ .nvim.lua
@@ -0,0 +1,24 @@
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        globals = { 'vim' },
+      },
+      hint = {
+        enable = true,
+        arrayIndex = 'Disable',
+      },
+      workspace = {
+        library = { vim.env.VIMRUNTIME },
+        checkThirdParty = false,
+      },
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+})
+vim.lsp.enable('lua_ls')
