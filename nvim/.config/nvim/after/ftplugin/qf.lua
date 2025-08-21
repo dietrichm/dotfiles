2771c208e116073c404b9a1afe1b1e467ac8c27b
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-08-21 12:38:36 +0200

    Have working filter mappings for location lists too

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -5,9 +5,22 @@ vim.opt_local.signcolumn = 'auto'
 vim.opt_local.statusline = [[ %t%<%( %{get(w:, 'quickfix_title', '')}%) %=%l/%L ]]
 vim.opt_local.wrap = false
 
-vim.keymap.set('n', "'", [[:Cfilter ]], { buffer = true })
-vim.keymap.set('n', '!', [[:Cfilter! ]], { buffer = true })
-vim.keymap.set('n', '^', [[:Cfilter ^]], { buffer = true })
+local winid = vim.api.nvim_get_current_win()
+local function nmap(lhs, rhs)
+  vim.keymap.set('n', lhs, rhs, { buffer = true })
+end
+
+if vim.fn.getwininfo(winid)[1].loclist == 1 then
+  nmap("'", [[:Lfilter ]])
+  nmap('!', [[:Lfilter! ]])
+  nmap('^', [[:Lfilter ^]])
+  nmap('u', [[:lolder<CR>]])
+else
+  nmap("'", [[:Cfilter ]])
+  nmap('!', [[:Cfilter! ]])
+  nmap('^', [[:Cfilter ^]])
+  nmap('u', [[:colder<CR>]])
+end
 
 if vim.w.quickfix_title ~= nil then
   local grepprg = vim.o.grepprg:gsub('%-', '%%-')
