92bdf6ef18141ef8f0b852a16e6e9cf159be1b31
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-08-30 12:44:39 +0200

    Split explicitly in quickfix/location list with <C-v>

--- nvim/.config/nvim/after/ftplugin/qf.lua
+++ nvim/.config/nvim/after/ftplugin/qf.lua
@@ -5,12 +5,12 @@ vim.opt_local.signcolumn = 'auto'
 vim.opt_local.statusline = [[ %t%<%( %{get(w:, 'quickfix_title', '')}%) %=%l/%L ]]
 vim.opt_local.wrap = false
 
-local winid = vim.api.nvim_get_current_win()
+local is_loclist = vim.fn.getwininfo(vim.api.nvim_get_current_win())[1].loclist == 1
 local function nmap(lhs, rhs)
   vim.keymap.set('n', lhs, rhs, { buffer = true })
 end
 
-if vim.fn.getwininfo(winid)[1].loclist == 1 then
+if is_loclist then
   nmap("'", [[:Lfilter ]])
   nmap('!', [[:Lfilter! ]])
   nmap('^', [[:Lfilter ^]])
@@ -21,6 +21,20 @@ else
   nmap('^', [[:Cfilter ^]])
   nmap('u', [[:colder<CR>]])
 end
+
+nmap('<C-v>', function()
+  local switchbuf_old = vim.o.switchbuf
+  local index = vim.fn.line('.')
+  vim.cmd.wincmd('p')
+  vim.cmd.vsplit()
+  vim.o.switchbuf = 'uselast'
+  if is_loclist then
+    vim.cmd.ll(index)
+  else
+    vim.cmd.cc(index)
+  end
+  vim.o.switchbuf = switchbuf_old
+end)
 
 if vim.w.quickfix_title ~= nil then
   local grepprg = vim.o.grepprg:gsub('%-', '%%-')
