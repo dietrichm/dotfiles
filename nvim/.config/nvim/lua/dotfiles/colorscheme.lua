a80af28e3ea9d93aefdac2b2afcbf860fcf714f3
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-09-11 20:39:24 +0200

    Clean-up `colorscheme.lua`

--- nvim/.config/nvim/lua/dotfiles/colorscheme.lua
+++ nvim/.config/nvim/lua/dotfiles/colorscheme.lua
@@ -1,22 +1,22 @@
-local augroup = vim.api.nvim_create_augroup('dotfiles_colorscheme', { clear = true })
-local hi = vim.cmd.highlight
-
 vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = 'default',
-  group = augroup,
+  group = vim.api.nvim_create_augroup('dotfiles_colorscheme', { clear = true }),
   callback = function()
+    local hi = vim.cmd.highlight
+
     hi { 'DiagnosticUnderlineError', 'gui=undercurl' }
     hi { 'link', 'ColorColumn', 'CursorLine', bang = true }
     hi { 'TreesitterContextBottom', 'gui=underline', 'guisp=NvimDarkGrey4' }
-    if vim.env.TERM == 'xterm-kitty' then
-      hi { '@comment', 'guifg=NvimLightGrey4', 'gui=italic' }
-    end
     hi { 'link', 'LspInlayHint', '@comment' }
     hi { 'Added', 'guifg=NvimLightBlue' }
     hi { 'Changed', 'guifg=NvimLightYellow' }
     hi { 'FloatBorder', 'guifg=NvimDarkGrey4', 'guibg=NvimDarkGrey1' }
     hi { 'qfText', 'guifg=NvimLightGrey2' }
     hi { 'MiniPickMatchRanges', 'gui=underline', 'guifg=NvimLightYellow' }
+
+    if vim.env.TERM == 'xterm-kitty' then
+      hi { '@comment', 'guifg=NvimLightGrey4', 'gui=italic' }
+    end
   end,
 })
 
