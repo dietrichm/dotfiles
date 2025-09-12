85ef83b269c1cea852fac2d4091222ff807107e9
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-09-12 17:52:21 +0200

    Reformat `formatters.lua`

--- nvim/.config/nvim/lua/dotfiles/formatters.lua
+++ nvim/.config/nvim/lua/dotfiles/formatters.lua
@@ -5,48 +5,25 @@ end
 
 conform.setup {
   formatters_by_ft = {
-    go = {
-      'gofumpt',
-      'gofmt',
-      stop_after_first = true,
-    },
-    php = {
-      'pint',
-      'php_cs_fixer',
-      stop_after_first = true,
-    },
-    javascript = {
-      'prettier',
-    },
-    lua = {
-      'stylua',
-    },
-    python = {
-      'isort',
-      'black',
-    },
-    typescript = {
-      'prettier',
-    },
-    typescriptreact = {
-      'prettier',
-    },
+    go = { 'gofumpt', 'gofmt', stop_after_first = true },
+    javascript = { 'prettier' },
+    lua = { 'stylua' },
+    php = { 'pint', 'php_cs_fixer', stop_after_first = true },
+    python = { 'isort', 'black' },
+    typescript = { 'prettier' },
+    typescriptreact = { 'prettier' },
   },
   format_on_save = function(bufnr)
     local opts = vim.bo[bufnr]
-
     if opts.buftype ~= '' then
       return
     end
-
     if not opts.modifiable then
       return
     end
-
     if opts.readonly then
       return
     end
-
     return {
       lsp_format = 'fallback',
       timeout_ms = 3000,
