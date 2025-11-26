b7f8cf93aca8ba9dbc05717bf225d54c723e1e15
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-11-26 10:25:59 +0100

    Make current statusline more readable

--- nvim/.config/nvim/lua/dotfiles/colorscheme.lua
+++ nvim/.config/nvim/lua/dotfiles/colorscheme.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd('ColorScheme', {
       hi { 'MiniPickMatchRanges', 'guifg=NvimLightYellow' }
       hi { 'NonText', 'guifg=grey' }
       hi { 'qfText', 'guifg=NvimLightGrey2' }
+      hi { 'StatusLine', 'gui=bold', 'guifg=NvimLightGrey1', 'guibg=#707070' }
       hi { 'TreesitterContextBottom', 'guisp=NvimDarkGrey4' }
     end
   end,
