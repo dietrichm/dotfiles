f728ecb84ddbee7634bdba0fc1da24033db79f14
Dietrich Moerman <dietrich.moerman@gmail.com>
2025-12-31 19:07:05 +0100

    Add newline before non-first zsh prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -4,3 +4,14 @@
 PROMPT='%F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
 RPROMPT='%F{#9a9da4}%*%f'
 ZLE_RPROMPT_INDENT=0
+
+function _dotfiles_prompt_newline() {
+    if [ -z "$_dotfiles_first_prompt_done" ]; then
+        _dotfiles_first_prompt_done=1
+        return
+    fi
+    echo
+}
+
+typeset -ag precmd_functions
+precmd_functions+=(_dotfiles_prompt_newline)
