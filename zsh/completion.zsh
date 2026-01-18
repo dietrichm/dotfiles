9e75c8481b6a692a50d4147715a9e5485aea5066
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-01-18 18:28:20 +0100

    Skip early when fzf-tab is not present

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -1,6 +1,12 @@
+if [ ! -f "$MY_CONFIG_ROOT/vendor/fzf-tab/fzf-tab.plugin.zsh" ]; then
+    return
+fi
+
 autoload -U compinit
 compinit
-[ -f "$MY_CONFIG_ROOT/vendor/fzf-tab/fzf-tab.plugin.zsh" ] && source "$MY_CONFIG_ROOT/vendor/fzf-tab/fzf-tab.plugin.zsh"
+
+source "$MY_CONFIG_ROOT/vendor/fzf-tab/fzf-tab.plugin.zsh"
+
 zstyle ':completion:*:git-checkout:*' sort false
 zstyle ':completion:*:descriptions' format '[%d]'
 zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
