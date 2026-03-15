6f0753e64924ce884c80f008088de63eddfe72a0
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-03-15 18:45:32 +0100

    Remove unused `loadkey` shell function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -1,13 +1,3 @@
-loadkey() {
-    local key="id_rsa"
-
-    if [ $# -ge 1 ]; then
-        key="$1"
-    fi
-
-    SSH_ASKPASS=ksshaskpass ssh-add ~/.ssh/"$key" < /dev/null
-}
-
 pdfcompress() {
     if [ $# -lt 2 ]; then
         echo "Usage: $0 <input-file> <output-file>"
