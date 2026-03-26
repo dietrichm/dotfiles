111d03784fd336a8bfdc6201d4902481bb0c9c1f
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-03-26 22:49:48 +0100

    Limit LSP ins-completion item to completion label without details

--- nvim/.config/nvim/lua/dotfiles/completion.lua
+++ nvim/.config/nvim/lua/dotfiles/completion.lua
@@ -10,7 +10,12 @@ if vim.env.NVIM_COMPLETION == '1' then
   vim.api.nvim_create_autocmd('LspAttach', {
     group = vim.api.nvim_create_augroup('dotfiles_completion', { clear = true }),
     callback = function(args)
-      vim.lsp.completion.enable(true, args.data.client_id, args.buf, { autotrigger = true })
+      vim.lsp.completion.enable(true, args.data.client_id, args.buf, {
+        autotrigger = true,
+        convert = function(item)
+          return { abbr = item.label }
+        end,
+      })
       vim.keymap.set('i', '<C-Space>', vim.lsp.completion.get, { buffer = args.buf })
     end,
   })
