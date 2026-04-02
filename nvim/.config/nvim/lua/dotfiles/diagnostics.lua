5e3d239c7f5ae21dde616b36c7676da0d024da82
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-04-02 17:53:58 +0200

    Format diagnostics in statusline as ❌1 ⚠️2 ℹ️1 💡3

--- nvim/.config/nvim/lua/dotfiles/diagnostics.lua
+++ nvim/.config/nvim/lua/dotfiles/diagnostics.lua
@@ -1,13 +1,24 @@
 local diagnostic = vim.diagnostic
 
+local signs = {
+  [diagnostic.severity.ERROR] = '❌',
+  [diagnostic.severity.WARN] = '⚠️',
+  [diagnostic.severity.INFO] = 'ℹ️',
+  [diagnostic.severity.HINT] = '💡',
+}
+
 diagnostic.config {
   signs = {
-    text = {
-      [diagnostic.severity.ERROR] = '❌',
-      [diagnostic.severity.WARN] = '⚠️',
-      [diagnostic.severity.INFO] = 'ℹ️',
-      [diagnostic.severity.HINT] = '💡',
-    },
+    text = signs,
+  },
+  status = {
+    format = function(counts)
+      local parts = {}
+      for severity, count in pairs(counts) do
+        table.insert(parts, signs[severity] .. count)
+      end
+      return table.concat(parts, ' ')
+    end,
   },
   virtual_text = {
     current_line = true,
