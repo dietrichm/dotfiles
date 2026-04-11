f41c689cdadfd7026a11e8c4cd93baf0e737d171
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-04-11 16:06:06 +0200

    Use `nvim_set_hl{update:boolean}` instead of `vim.cmd.highlight`

--- nvim/.config/nvim/lua/dotfiles/colorscheme.lua
+++ nvim/.config/nvim/lua/dotfiles/colorscheme.lua
@@ -2,23 +2,26 @@ vim.api.nvim_create_autocmd('ColorScheme', {
   pattern = 'default',
   group = vim.api.nvim_create_augroup('dotfiles_colorscheme', { clear = true }),
   callback = function()
-    local hi = vim.cmd.highlight
+    local hi = function(name, val)
+      val.update = true
+      vim.api.nvim_set_hl(0, name, val)
+    end
 
-    hi { 'DiagnosticUnderlineError', 'gui=undercurl' }
-    hi { 'link', 'ColorColumn', 'CursorLine', bang = true }
-    hi { 'link', 'Sneak', 'Search', bang = true }
-    hi { 'link', 'SneakCurrent', 'CurSearch', bang = true }
-    hi { 'MiniPickMatchRanges', 'gui=underline' }
-    hi { 'TreesitterContextBottom', 'gui=underline' }
+    hi('ColorColumn', { link = 'CursorLine' })
+    hi('DiagnosticUnderlineError', { undercurl = true })
+    hi('MiniPickMatchRanges', { underline = true })
+    hi('Sneak', { link = 'Search' })
+    hi('SneakCurrent', { link = 'CurSearch' })
+    hi('TreesitterContextBottom', { underline = true })
 
     if vim.o.background == 'dark' then
-      hi { 'Added', 'guifg=NvimLightBlue' }
-      hi { 'Changed', 'guifg=NvimLightYellow' }
-      hi { 'FloatBorder', 'guifg=NvimDarkGrey4', 'guibg=NvimDarkGrey2' }
-      hi { 'MiniPickMatchRanges', 'guifg=NvimLightYellow' }
-      hi { 'Normal', 'guibg=Black' }
-      hi { 'NormalFloat', 'guibg=NvimDarkGrey2' }
-      hi { 'TreesitterContextBottom', 'guisp=NvimDarkGrey4' }
+      hi('Added', { fg = 'NvimLightBlue' })
+      hi('Changed', { fg = 'NvimLightYellow' })
+      hi('FloatBorder', { fg = 'NvimDarkGrey4', bg = 'NvimDarkGrey2' })
+      hi('MiniPickMatchRanges', { fg = 'NvimLightYellow' })
+      hi('Normal', { bg = 'Black' })
+      hi('NormalFloat', { bg = 'NvimDarkGrey2' })
+      hi('TreesitterContextBottom', { sp = 'NvimDarkGrey4' })
     end
   end,
 })
