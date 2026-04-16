aa7718875b98d1fa6a8bdeb5204e1ef9cd4b930b
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-04-16 10:01:45 +0200

    Have :Only that takes new file to edit

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -79,6 +79,7 @@ vim.api.nvim_create_user_command('Grep', [[:silent grep! <args>]], { nargs = '+'
 vim.api.nvim_create_user_command('LspInfo', [[:checkhealth vim.lsp]], {})
 vim.api.nvim_create_user_command('Nw', [[:noautocmd w]], {})
 vim.api.nvim_create_user_command('Nwa', [[:noautocmd wa]], {})
+vim.api.nvim_create_user_command('Only', [[:only | :e <args>]], { nargs = 1, complete = 'file' })
 vim.api.nvim_create_user_command('PackUpdate', [[:lua vim.pack.update()]], {})
 
 require('dotfiles.colorscheme')
