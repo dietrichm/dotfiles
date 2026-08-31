b0efae7156f6198955ee130bd3c82077e059b2b9
Dietrich Moerman <dietrich.moerman@gmail.com>
2026-08-31 09:04:17 +0200

    Remove right prompt and blank line between commands

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -3,21 +3,3 @@
 # NvimLightGrey4: #9a9da4
 
 PROMPT='%F{#a6dbff}%~ %B%F{#b3f6c0}%#%f%b '
-
-if [ -n "$MC_SID" ]; then
-    return
-fi
-
-RPROMPT='%F{#9a9da4}%*%f'
-ZLE_RPROMPT_INDENT=0
-
-function _dotfiles_prompt_newline() {
-    if [ -z "$_dotfiles_first_prompt_done" ]; then
-        _dotfiles_first_prompt_done=1
-        return
-    fi
-    printf "\n"
-}
-
-typeset -ag precmd_functions
-precmd_functions+=(_dotfiles_prompt_newline)
